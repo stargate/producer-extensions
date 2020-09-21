c88f8a8fae548d62de321602c4a7262860527ddb
tomekl007 <tomasz.lelek@datastax.com>
2020-09-21 11:54:59 +0200

    create application skeleton

--- kafka/src/main/java/io/stargate/producer/kafka/KafkaProducerActivator.java
+++ kafka/src/main/java/io/stargate/producer/kafka/KafkaProducerActivator.java
@@ -1,4 +1,24 @@
 package io.stargate.producer.kafka;
 
-public class KafkaProducerActivator {
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+
+/* Logic for registering the kafka producer as an OSGI bundle */
+public class KafkaProducerActivator implements BundleActivator, ServiceListener {
+	@Override
+	public void start(BundleContext context) throws Exception {
+
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+
+	}
+
+	@Override
+	public void serviceChanged(ServiceEvent event) {
+
+	}
 }
--- kafka/src/test/java/io/stargate/producer/kafka/SampleTest.java
+++ kafka/src/test/java/io/stargate/producer/kafka/SampleTest.java
@@ -1,4 +1,13 @@
 package io.stargate.producer.kafka;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class SampleTest {
+
+	@Test
+	void shouldAddTwoNumbers(){
+		assertThat(1+1).isEqualTo(2);
+	}
 }
